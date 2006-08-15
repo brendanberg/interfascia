d27bd1d38e315559d4a3313c9b51aa3c0d85bf79
Brendan Berg <brendan@plusminusfive.com>
2006-08-15 19:07:37 +0000

    Corrected a problem that prevents more than 10 items to be added to the radio controller
    git-svn-id: https://interfascia.googlecode.com/svn@7 6cc53eec-651a-0410-8be0-87c60f18f364

--- trunk/IFRadioController.java
+++ trunk/IFRadioController.java
@@ -1,25 +1,100 @@
-// Interfascia -- ALPHA 001
+// Interfascia ALPHA 002 -- http://superstable.net/interfascia/
+// GUI Library for Processing -- http://www.processing.org/
 //
-// A graphical user interface library for the
-// Processing environment.
+// Copyright (C) 2006 Brendan Berg
+// interfascia (at) thbbpt (dot) net
 //
-// by Brendan Berg
+// This library is free software; you can redistribute it and/or 
+// modify it under the terms of the GNU Lesser General Public 
+// License as published by the Free Software Foundation; either 
+// version 2.1 of the License, or (at your option) any later version.
 //
-// This software is released under the LGPL?
+// This library is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public 
+// License along with this library; if not, write to the Free Software 
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+
+
 
 package interfascia;
 import processing.core.*;
 import java.lang.reflect.*;
 
-public class IFRadioController extends GUIComponent {	private IFRadioButton[] contents;	private int numItems = 0, selected = -1;	private String label;	private Object listener;	public IFRadioController() {		contents = new IFRadioButton[10];	}	public IFRadioController (String argLabel) {		label = argLabel;		contents = new IFRadioButton[10];	}
+public class IFRadioController extends GUIComponent {
+	private IFRadioButton[] contents;
+	private int numItems = 0, selected = -1;
+	private String label;
+	private Object listener;
+
+	public IFRadioController() {
+		contents = new IFRadioButton[5];
+	}
+
+	public IFRadioController (String argLabel) {
+		label = argLabel;
+		contents = new IFRadioButton[5];
+	}
 	
 	public IFRadioController (String argLabel, Object l) {
 		label = argLabel;
-		listener = l;
-		contents = new IFRadioButton[10];
-	}	public void add (IFRadioButton button) {		contents[numItems++] = button;	}	public void remove (IFRadioButton button) {	}	public int getSelectedIndex () {		return selected;	}	public IFRadioButton getSelected () {		if (selected >= 0 && selected < numItems) {			return contents[selected];		} else {			return null;		}	}	public void buttonPressed (IFRadioButton button) {		for (int i = 0; i < numItems; i++) {			contents[i].setSelected (false);			if (contents[i] == button)			selected = i;		}		button.setSelected (true);		fireEventNotification (button, "Selected");	}
+		addActionListener(l);
+		contents = new IFRadioButton[5];
+	}
+
+	public void add (IFRadioButton button) {
+		if (numItems == contents.length) {
+			IFRadioButton[] temp = contents;
+			contents = new IFRadioButton[contents.length * 2];
+			System.arraycopy(temp, 0, contents, 0, numItems);
+		}
+		
+		contents[numItems++] = button;
+	}
+
+	public void remove (IFRadioButton button) {
+
+	}
+
+	public int getSelectedIndex () {
+		return selected;
+	}
+
+	public IFRadioButton getSelected () {
+		if (selected >= 0 && selected < numItems) {
+			return contents[selected];
+		} else {
+			return null;
+		}
+	}
+
+	public void buttonPressed (IFRadioButton button) {
+		for (int i = 0; i < numItems; i++) {
+			contents[i].setSelected (false);
+			if (contents[i] == button)
+			selected = i;
+		}
+		button.setSelected (true);
+		fireEventNotification (button, "Selected");
+	}
 	
 	public void setSelectedButton (IFRadioButton button) {
 		buttonPressed(button);
-	}	public void deselectAll () {		for (int i = 0; i < numItems; i++) {			contents[i].setSelected (false);		}		selected = -1;	}	public void addActionListener (Object arglistener) {		listener = arglistener;	}/*	public void fireEventNotification (GUIComponent argComponent, String argMessage) {		if (listener != null) {			try {				GUIEvent e = new GUIEvent(argComponent, argMessage);				Method m = listener.getClass().getDeclaredMethod("actionPerformed", new Class[] { e.getClass() });				try {					m.invoke(listener, new Object[] { e });				} catch (InvocationTargetException ex) {
-					System.out.println(ex.getCause().getMessage());				} catch (IllegalAccessException ex) {				}			} catch (NoSuchMethodException ex) {				System.out.println( "NoSuchMethodException" );			}		}	}*/}
+	}
+
+	public void deselectAll () {
+		for (int i = 0; i < numItems; i++) {
+			contents[i].setSelected (false);
+		}
+		selected = -1;
+	}
+
+	public void addActionListener (Object arglistener) {
+		listener = arglistener;
+	}
+
+}
