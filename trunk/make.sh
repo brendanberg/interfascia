258940a0c159beb4b347fb33129c73b193893a9d
Brendan Berg <brendan@plusminusfive.com>
2006-08-16 03:50:54 +0000

    Updated make.sh
    git-svn-id: https://interfascia.googlecode.com/svn@11 6cc53eec-651a-0410-8be0-87c60f18f364

--- trunk/make.sh
+++ trunk/make.sh
@@ -8,4 +8,4 @@ fi
 rm -rf interfascia
 rm interfascia.jar
 javac -source 1.3 -target 1.1 -d . -classpath "$PROCESSING_PATH/lib/core.jar" *.java
-jar -cf interfascia.jar interfascia
+jar -cf interfascia.jar interfascia data
