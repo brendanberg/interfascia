44c8673dee3a2c5f81fea315f75bab3a40076ff9
Nicola Bernardini <nicb@sme-ccppd.org>
2015-12-16 17:54:15 +0100

    stubbed out code for IFTextField, IFCheckBox, IFRadioButton (needs to be refactored for Processing 3)

--- interfascia/IFRadioButton.java
+++ interfascia/IFRadioButton.java
@@ -23,90 +23,92 @@
 
 package interfascia;
 
-import java.awt.event.*;
+import processing.event.*;
 
 public class IFRadioButton extends GUIComponent {
-	private int currentColor;
-	private IFRadioController radioController;
+  private int currentColor;
+  private IFRadioController radioController;
 
-	public IFRadioButton (String newLabel, int newX, int newY, IFRadioController newController) {
-		setLabel(newLabel);
-		setPosition(newX, newY + 1);
-		setSize(15, 15);
+  public IFRadioButton (String newLabel, int newX, int newY, IFRadioController newController) {
+    setLabel(newLabel);
+    setPosition(newX, newY + 1);
+    setSize(15, 15);
 
-		radioController = newController;
-		radioController.add (this);
-	}
+    radioController = newController;
+    radioController.add (this);
+  }
 
-	public void initWithParent () {
-		controller.parent.registerMethod("mouseEvent", this);
-		
-		if (lookAndFeel == null)
-			return;
-		
-		controller.userState.saveSettingsForApplet(controller.parent);
-		lookAndFeel.defaultGraphicsState.restoreSettingsToApplet(controller.parent);
-		
-		setSize((int) Math.ceil(controller.parent.textWidth(getLabel())) + getHeight() + 5, 14);
+  public void initWithParent () {
+    controller.parent.registerMethod("mouseEvent", this);
+    
+    if (lookAndFeel == null)
+      return;
+    
+    controller.userState.saveSettingsForApplet(controller.parent);
+    lookAndFeel.defaultGraphicsState.restoreSettingsToApplet(controller.parent);
+    
+    setSize((int) Math.ceil(controller.parent.textWidth(getLabel())) + getHeight() + 5, 14);
 
-		controller.userState.restoreSettingsToApplet(controller.parent);
-	}
+    controller.userState.restoreSettingsToApplet(controller.parent);
+  }
 
-	public void mouseEvent(MouseEvent e) {
-		if (e.getID() == MouseEvent.MOUSE_PRESSED) {
-			if (isMouseOver (e.getX(), e.getY())) {
-				wasClicked = true;
-			}
-		} else if (e.getID() == MouseEvent.MOUSE_RELEASED) {
-			if (wasClicked && isMouseOver (e.getX(), e.getY())) {
-				radioController.selectButton(this);
-				wasClicked = false;
-			}
-		}
-	}
-	
-	public void keyEvent(KeyEvent e) {
-		if (e.getID() == KeyEvent.KEY_TYPED && e.getKeyChar() == ' ') {
-			radioController.selectButton(this);
-		}
-	}
+  public void mouseEvent(MouseEvent e) {
+// TODO: this is to be refactored with Processing 3 Events
+//  if (e.getID() == MouseEvent.MOUSE_PRESSED) {
+//    if (isMouseOver (e.getX(), e.getY())) {
+//      wasClicked = true;
+//    }
+//  } else if (e.getID() == MouseEvent.MOUSE_RELEASED) {
+//    if (wasClicked && isMouseOver (e.getX(), e.getY())) {
+//      radioController.selectButton(this);
+//      wasClicked = false;
+//    }
+//  }
+  }
+  
+  public void keyEvent(KeyEvent e) {
+// TODO: this is to be refactored with Processing 3 Events
+//  if (e.getID() == KeyEvent.KEY_TYPED && e.getKeyChar() == ' ') {
+//    radioController.selectButton(this);
+//  }
+  }
 
-	public void draw () {
-		if (isMouseOver (controller.parent.mouseX, controller.parent.mouseY)) {
-			currentColor = lookAndFeel.highlightColor;
-		} else if (controller.getFocusStatusForComponent(this)) {
-			currentColor = lookAndFeel.highlightColor;
-		} else {
-			currentColor = lookAndFeel.baseColor;
-		}
+  public void draw () {
+    if (isMouseOver (controller.parent.mouseX, controller.parent.mouseY)) {
+      currentColor = lookAndFeel.highlightColor;
+    } else if (controller.getFocusStatusForComponent(this)) {
+      currentColor = lookAndFeel.highlightColor;
+    } else {
+      currentColor = lookAndFeel.baseColor;
+    }
 
-		int x = getX(), y = getY(), hgt = getHeight(), wid = getWidth();
-		
-		controller.parent.stroke(lookAndFeel.borderColor);
-		controller.parent.fill(currentColor);
-		
-		controller.parent.ellipse(x, y, hgt, hgt);
-		if (radioController.getSelectionStatusForButton(this)) {
-			controller.parent.fill (lookAndFeel.lightGrayColor);
-			controller.parent.ellipse (x + 2, y + 2, hgt - 4, hgt - 4);
-		}
-		
-		controller.parent.fill (lookAndFeel.textColor);
-		controller.parent.text (getLabel(), hgt + x + 5, (hgt - 3) + y);
-		
-		if (controller.showBounds) {
-			controller.parent.noFill();
-			controller.parent.stroke(255,0,0);
-			controller.parent.rect(x, y, wid, hgt);
-		}
-	}
+    int x = getX(), y = getY(), hgt = getHeight(), wid = getWidth();
+    
+    controller.parent.stroke(lookAndFeel.borderColor);
+    controller.parent.fill(currentColor);
+    
+    controller.parent.ellipse(x, y, hgt, hgt);
+    if (radioController.getSelectionStatusForButton(this)) {
+      controller.parent.fill (lookAndFeel.lightGrayColor);
+      controller.parent.ellipse (x + 2, y + 2, hgt - 4, hgt - 4);
+    }
+    
+    controller.parent.fill (lookAndFeel.textColor);
+    controller.parent.text (getLabel(), hgt + x + 5, (hgt - 3) + y);
+    
+    if (controller.showBounds) {
+      controller.parent.noFill();
+      controller.parent.stroke(255,0,0);
+      controller.parent.rect(x, y, wid, hgt);
+    }
+  }
 
-	public boolean isSelected () {
-		return radioController.getSelectionStatusForButton(this);
-	}
+  public boolean isSelected () {
+    return radioController.getSelectionStatusForButton(this);
+  }
 
-	public void setSelected () {
-		radioController.selectButton(this);
-	}
+  public void setSelected () {
+    radioController.selectButton(this);
+  }
 
 }
