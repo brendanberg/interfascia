2012ed8ba75cd2cb291e87bf2afa52172af0bfe6
Nicola Bernardini <nicb@sme-ccppd.org>
2015-12-16 13:05:50 +0100

    stubbed out code for IFButton (needs to be refactored for Processing 3)

--- interfascia/IFButton.java
+++ interfascia/IFButton.java
@@ -23,76 +23,77 @@
 
 package interfascia;
 import processing.core.*;
-
-import java.awt.event.*;
+import processing.event.*;
 
 public class IFButton extends GUIComponent {
-	private int currentColor;
+  private int currentColor;
 
-	public IFButton (String newLabel, int newX, int newY) {
-		this (newLabel, newX, newY, 100, 21);
-	}
+  public IFButton (String newLabel, int newX, int newY) {
+    this (newLabel, newX, newY, 100, 21);
+  }
 
-	public IFButton (String newLabel, int newX, int newY, int newWidth) {
-		this (newLabel, newX, newY, newWidth, 21);
-	}
+  public IFButton (String newLabel, int newX, int newY, int newWidth) {
+    this (newLabel, newX, newY, newWidth, 21);
+  }
 
-	public IFButton (String newLabel, int newX, int newY, int newWidth, int newHeight) {
-		setLabel(newLabel);
-		setPosition(newX, newY);
-		setSize(newWidth, newHeight);
-	}
+  public IFButton (String newLabel, int newX, int newY, int newWidth, int newHeight) {
+    setLabel(newLabel);
+    setPosition(newX, newY);
+    setSize(newWidth, newHeight);
+  }
 
-	public void initWithParent () {
-		controller.parent.registerMethod("mouseEvent", this);
-	}
+  public void initWithParent () {
+    controller.parent.registerMethod("mouseEvent", this);
+  }
 
-	public void mouseEvent(MouseEvent e) {
-		if (e.getID() == MouseEvent.MOUSE_PRESSED) {
-			if (isMouseOver (e.getX(), e.getY())) {
-				wasClicked = true;
-			}
-		} else if (e.getID() == MouseEvent.MOUSE_RELEASED) {
-			if (wasClicked && isMouseOver (e.getX(), e.getY())) {
-				fireEventNotification(this, "Clicked");
-				wasClicked = false;
-			}
-		}
-	}
+  public void mouseEvent(MouseEvent e) {
+// TODO: this is to be refactored with Processing 3 Events
+//  if (e.getID() == MouseEvent.MOUSE_PRESSED) {
+//    if (isMouseOver (e.getX(), e.getY())) {
+//      wasClicked = true;
+//    }
+//  } else if (e.getID() == MouseEvent.MOUSE_RELEASED) {
+//    if (wasClicked && isMouseOver (e.getX(), e.getY())) {
+//      fireEventNotification(this, "Clicked");
+//      wasClicked = false;
+//    }
+//  }
+  }
 
-	public void draw () {
-		boolean hasFocus = controller.getFocusStatusForComponent(this);
-	
-		if (wasClicked) {
-			 currentColor = lookAndFeel.activeColor;
-		} else if (isMouseOver (controller.parent.mouseX, controller.parent.mouseY) || hasFocus) {
-			 currentColor = lookAndFeel.highlightColor;
-		} else {
-			 currentColor = lookAndFeel.baseColor;
-		}
+  public void draw () {
+    boolean hasFocus = controller.getFocusStatusForComponent(this);
+  
+    if (wasClicked) {
+       currentColor = lookAndFeel.activeColor;
+    } else if (isMouseOver (controller.parent.mouseX, controller.parent.mouseY) || hasFocus) {
+       currentColor = lookAndFeel.highlightColor;
+    } else {
+       currentColor = lookAndFeel.baseColor;
+    }
 
-		int x = getX(), y = getY(), hgt = getHeight(), wid = getWidth();
-	
-		controller.parent.stroke(lookAndFeel.borderColor);
-		controller.parent.fill(currentColor);
-		controller.parent.rect(x, y, wid, hgt);
-		controller.parent.fill (lookAndFeel.textColor);
+    int x = getX(), y = getY(), hgt = getHeight(), wid = getWidth();
+  
+    controller.parent.stroke(lookAndFeel.borderColor);
+    controller.parent.fill(currentColor);
+    controller.parent.rect(x, y, wid, hgt);
+    controller.parent.fill (lookAndFeel.textColor);
 
-		controller.parent.textAlign (PApplet.CENTER);
-		controller.parent.text (getLabel(), x, y + 3, wid, hgt);
-		controller.parent.textAlign (PApplet.LEFT);
+    controller.parent.textAlign (PApplet.CENTER);
+    controller.parent.text (getLabel(), x, y + 3, wid, hgt);
+    controller.parent.textAlign (PApplet.LEFT);
 
-		if (controller.showBounds) {
-			controller.parent.noFill();
-			controller.parent.stroke(255,0,0);
-			controller.parent.rect(x, y, wid, hgt);
-		}
-	}
-		
-	public void keyEvent(KeyEvent e) {
-		if (e.getID() == KeyEvent.KEY_TYPED && e.getKeyChar() == ' ') {
-			fireEventNotification(this, "Selected");
-		}
-	}
+    if (controller.showBounds) {
+      controller.parent.noFill();
+      controller.parent.stroke(255,0,0);
+      controller.parent.rect(x, y, wid, hgt);
+    }
+  }
+    
+  public void keyEvent(KeyEvent e) {
+// TODO: this is to be refactored with Processing 3 Events
+//  if (e.getID() == KeyEvent.KEY_TYPED && e.getKeyChar() == ' ') {
+//    fireEventNotification(this, "Selected");
+//  }
+  }
 
 }
